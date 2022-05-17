f399cf5d2667be093d0ff43b20f7eee7984335b0
aditya-adiga <adityaadiga6@gmail.com>
2022-05-17 09:00:29 +0530

    added frontend of groupchat

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script>
   import Chat from './Chat.svelte';
+  import Chat2 from './Chat2.svelte';
   import Header from './Header.svelte';
 </script>
 
 <div class="app">
     <Header />
-    <Chat />
+    <Chat2 />
 </div>
---
+++ src/Chat2.svelte
@@ -0,0 +1,163 @@
+<style>
+.gradient-custom {
+/* fallback for old browsers */
+background: #fccb90;
+
+/* Chrome 10-25, Safari 5.1-6 */
+background: -webkit-linear-gradient(to bottom right, rgba(252, 203, 144, 1), rgba(213, 126, 235, 1));
+
+/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+background: linear-gradient(to bottom right, rgba(252, 203, 144, 1), rgba(213, 126, 235, 1))
+}
+
+.mask-custom {
+background: rgba(24, 24, 16, .2);
+border-radius: 2em;
+backdrop-filter: blur(15px);
+border: 2px solid rgba(255, 255, 255, 0.05);
+background-clip: padding-box;
+box-shadow: 10px 10px 10px rgba(46, 54, 68, 0.03);
+}
+.sub-menu {
+    padding: 0;
+    max-height: 40em; /* 1.5 x 3 */
+    overflow-y: auto;
+}
+</style>
+<div class="container py-5">
+
+    <div class="row">
+
+      <div class="col-md-6 col-lg-6 col-xl-12 mb-6 mb-md-0">
+
+        <div class="card mask-custom">
+          <div class="card-body">
+
+            <ul class="list-unstyled mb-0 sub-menu">
+              <li class="p-2 border-bottom" style="border-bottom: 1px solid rgba(255,255,255,.3) !important;">
+                <a href="#!" class="d-flex justify-content-between link-light">
+                  <div class="d-flex flex-row">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-8.webp" alt="avatar"
+                      class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
+                    <div class="pt-1">
+                      <p class="fw-bold mb-0">John Doe</p>
+                      <p class="small text-white">Hello, Are you there?</p>
+                    </div>
+                  </div>
+                  <div class="pt-1">
+                    <p class="small text-white mb-1">Just now</p>
+                    <span class="badge bg-danger float-end">1</span>
+                  </div>
+                </a>
+              </li>
+              <li class="p-2 border-bottom" style="border-bottom: 1px solid rgba(255,255,255,.3) !important;">
+                <a href="#!" class="d-flex justify-content-between link-light">
+                  <div class="d-flex flex-row">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp" alt="avatar"
+                      class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
+                    <div class="pt-1">
+                      <p class="fw-bold mb-0">Danny Smith</p>
+                      <p class="small text-white">Lorem ipsum dolor sit.</p>
+                    </div>
+                  </div>
+                  <div class="pt-1">
+                    <p class="small text-white mb-1">5 mins ago</p>
+                  </div>
+                </a>
+              </li>
+              <li class="p-2 border-bottom" style="border-bottom: 1px solid rgba(255,255,255,.3) !important;">
+                <a href="#!" class="d-flex justify-content-between link-light">
+                  <div class="d-flex flex-row">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp" alt="avatar"
+                      class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
+                    <div class="pt-1">
+                      <p class="fw-bold mb-0">Danny Smith</p>
+                      <p class="small text-white">Lorem ipsum dolor sit.</p>
+                    </div>
+                  </div>
+                  <div class="pt-1">
+                    <p class="small text-white mb-1">5 mins ago</p>
+                  </div>
+                </a>
+              </li>
+              <li class="p-2 border-bottom" style="border-bottom: 1px solid rgba(255,255,255,.3) !important;">
+                <a href="#!" class="d-flex justify-content-between link-light">
+                  <div class="d-flex flex-row">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp" alt="avatar"
+                      class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
+                    <div class="pt-1">
+                      <p class="fw-bold mb-0">Danny Smith</p>
+                      <p class="small text-white">Lorem ipsum dolor sit.</p>
+                    </div>
+                  </div>
+                  <div class="pt-1">
+                    <p class="small text-white mb-1">5 mins ago</p>
+                  </div>
+                </a>
+              </li>
+              <li class="p-2 border-bottom" style="border-bottom: 1px solid rgba(255,255,255,.3) !important;">
+                <a href="#!" class="d-flex justify-content-between link-light">
+                  <div class="d-flex flex-row">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp" alt="avatar"
+                      class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
+                    <div class="pt-1">
+                      <p class="fw-bold mb-0">Danny Smith</p>
+                      <p class="small text-white">Lorem ipsum dolor sit.</p>
+                    </div>
+                  </div>
+                  <div class="pt-1">
+                    <p class="small text-white mb-1">5 mins ago</p>
+                  </div>
+                </a>
+              </li>
+              <li class="p-2 border-bottom" style="border-bottom: 1px solid rgba(255,255,255,.3) !important;">
+                <a href="#!" class="d-flex justify-content-between link-light">
+                  <div class="d-flex flex-row">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp" alt="avatar"
+                      class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
+                    <div class="pt-1">
+                      <p class="fw-bold mb-0">Danny Smith</p>
+                      <p class="small text-white">Lorem ipsum dolor sit.</p>
+                    </div>
+                  </div>
+                  <div class="pt-1">
+                    <p class="small text-white mb-1">5 mins ago</p>
+                  </div>
+                </a>
+              </li>
+              <li class="p-2 border-bottom" style="border-bottom: 1px solid rgba(255,255,255,.3) !important;">
+                <a href="#!" class="d-flex justify-content-between link-light">
+                  <div class="d-flex flex-row">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp" alt="avatar"
+                      class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
+                    <div class="pt-1">
+                      <p class="fw-bold mb-0">Danny Smith</p>
+                      <p class="small text-white">Lorem ipsum dolor sit.</p>
+                    </div>
+                  </div>
+                  <div class="pt-1">
+                    <p class="small text-white mb-1">5 mins ago</p>
+                  </div>
+                </a>
+              </li>
+              <li class="p-2 border-bottom" style="border-bottom: 1px solid rgba(255,255,255,.3) !important;">
+                <a href="#!" class="d-flex justify-content-between link-light">
+                  <div class="d-flex flex-row">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/avatar-1.webp" alt="avatar"
+                      class="rounded-circle d-flex align-self-center me-3 shadow-1-strong" width="60">
+                    <div class="pt-1">
+                      <p class="fw-bold mb-0">Danny Smith</p>
+                      <p class="small text-white">Lorem ipsum dolor sit.</p>
+                    </div>
+                  </div>
+                  <div class="pt-1">
+                    <p class="small text-white mb-1">5 mins ago</p>
+                  </div>
+                </a>
+              </li>
+              </ul>
+              </div>
+              </div>
+              </div>
+              </div>
+              </div>
